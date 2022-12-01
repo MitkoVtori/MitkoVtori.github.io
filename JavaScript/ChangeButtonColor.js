c00dbb1b47410544ce65d3a5d0f6036407d0b5fc
Mikto.Vtori <gamingplayerdm@gmail.com>
2022-12-01 15:07:17 +0200

    created the website

---
+++ JavaScript/ChangeButtonColor.js
@@ -0,0 +1,17 @@
+function ChangeHomeButtonColor() {
+ document.getElementById("homeButton").style.color = "#D00FFF";
+ document.getElementById("AboutButton").style.color = "white";
+ document.getElementById("RepoButton").style.color = "white";
+}
+
+function ChangeAboutButtonColor() {
+ document.getElementById("AboutButton").style.color = "#D00FFF";
+ document.getElementById("RepoButton").style.color = "white";
+ document.getElementById("homeButton").style.color = "white";
+}
+
+function ChangeRepoButtonColor() {
+ document.getElementById("RepoButton").style.color = "#D00FFF";
+ document.getElementById("homeButton").style.color = "white";
+ document.getElementById("AboutButton").style.color = "white";
+}
