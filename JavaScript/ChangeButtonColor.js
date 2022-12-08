7c0637ec94718828b9a856e6d6eafaf064ab5a6e
Mikto.Vtori <gamingplayerdm@gmail.com>
2022-12-08 10:03:22 +0200

    added certificates and onhover.css

--- JavaScript/ChangeButtonColor.js
+++ JavaScript/ChangeButtonColor.js
@@ -1,11 +1,20 @@
 function ChangeHomeButtonColor() {
  document.getElementById("homeButton").style.color = "#D00FFF";
  document.getElementById("AboutButton").style.color = "white";
+ document.getElementById("certificatesButton").style.color = "white";
  document.getElementById("RepoButton").style.color = "white";
 }
 
 function ChangeAboutButtonColor() {
  document.getElementById("AboutButton").style.color = "#D00FFF";
+ document.getElementById("certificatesButton").style.color = "white";
+ document.getElementById("RepoButton").style.color = "white";
+ document.getElementById("homeButton").style.color = "white";
+}
+
+function ChangeCertificatesButtonColor() {
+ document.getElementById("certificatesButton").style.color = "#D00FFF";
+ document.getElementById("AboutButton").style.color = "white";
  document.getElementById("RepoButton").style.color = "white";
  document.getElementById("homeButton").style.color = "white";
 }
@@ -14,4 +23,5 @@ function ChangeRepoButtonColor() {
  document.getElementById("RepoButton").style.color = "#D00FFF";
  document.getElementById("homeButton").style.color = "white";
  document.getElementById("AboutButton").style.color = "white";
+ document.getElementById("certificatesButton").style.color = "white";
 }
