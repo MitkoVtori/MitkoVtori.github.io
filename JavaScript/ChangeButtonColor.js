366fe26194bcae05dc26ccf8a8e83799bdca00f1
Mikto.Vtori <gamingplayerdm@gmail.com>
2022-12-10 17:04:02 +0200

    added flip certificate on hover effect, made the sections links scrollable(following you while scrolling), created section socials for social media. added tricky button, fliping card with link to the repo and View page source link in the section.

--- JavaScript/ChangeButtonColor.js
+++ JavaScript/ChangeButtonColor.js
@@ -3,12 +3,14 @@ function ChangeHomeButtonColor() {
  document.getElementById("AboutButton").style.color = "white";
  document.getElementById("certificatesButton").style.color = "white";
  document.getElementById("RepoButton").style.color = "white";
+ document.getElementById("SocialsButton").style.color = "white";
 }
 
 function ChangeAboutButtonColor() {
  document.getElementById("AboutButton").style.color = "#D00FFF";
  document.getElementById("certificatesButton").style.color = "white";
  document.getElementById("RepoButton").style.color = "white";
+ document.getElementById("SocialsButton").style.color = "white";
  document.getElementById("homeButton").style.color = "white";
 }
 
@@ -16,11 +18,21 @@ function ChangeCertificatesButtonColor() {
  document.getElementById("certificatesButton").style.color = "#D00FFF";
  document.getElementById("AboutButton").style.color = "white";
  document.getElementById("RepoButton").style.color = "white";
+ document.getElementById("SocialsButton").style.color = "white";
  document.getElementById("homeButton").style.color = "white";
 }
 
 function ChangeRepoButtonColor() {
  document.getElementById("RepoButton").style.color = "#D00FFF";
+ document.getElementById("SocialsButton").style.color = "white";
+ document.getElementById("homeButton").style.color = "white";
+ document.getElementById("AboutButton").style.color = "white";
+ document.getElementById("certificatesButton").style.color = "white";
+}
+
+function ChangeSocialsButtonColor() {
+ document.getElementById("SocialsButton").style.color = "#D00FFF";
+ document.getElementById("RepoButton").style.color = "white";
  document.getElementById("homeButton").style.color = "white";
  document.getElementById("AboutButton").style.color = "white";
  document.getElementById("certificatesButton").style.color = "white";
