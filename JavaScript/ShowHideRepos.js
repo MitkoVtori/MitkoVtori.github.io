cc7a9bf7b80c0d44d01d3dc70f18d43bb7c8cae8
Mikto.Vtori <gamingplayerdm@gmail.com>
2023-04-22 15:00:39 +0300

    Changed the entire website - CV

--- JavaScript/ShowHideRepos.js
+++
@@ -1,8 +0,0 @@
-function ShowHideRepos() {
-  var x = document.getElementById("repositories");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
---
+++ js/sleep.js
@@ -0,0 +1,4 @@
+setTimeout(() => {
+let name = document.getElementById('name');
+name.style.display = 'block';
+}, 2000);
